d774c2440ad7f5c14005ce506961e46f84938648
FrSantiago <frsantiago018@gmail.com>
2026-08-26 15:12:43 -0300

    INIT: inicio e configuracao de Projeto de Cafeteria, utilizando o react-native para o desenvolvimento em habilidades de desenvolvimento mobile

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import { Ionicons } from '@expo/vector-icons';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <View>
+          <Text>Cafe do Codigo</Text>
+          <Text>Seu cafe, uma linha por vez</Text>
+        </View>
+
+        <View>
+          <Ionicons name="person" size={20} color="#2f2d2c"></Ionicons>
+        </View>
+
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9F9F9',
+  },
+
+  header: {
+    width: '100%',
+    padding: 60,
+    paddingHorizontal: 24,
+    paddingTop: 60,
+    paddingBottom: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  
+
+});
