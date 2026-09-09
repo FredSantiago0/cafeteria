aa7cd9f995af91d86a16cce2b27074e25d6aeae6
Frederico B Santiago <frsantiago018@gmail.com>
2026-09-09 15:06:01 -0300

    feat: add de Card e estilizacao da interface inicial
    Estilização dos textos do header e dos subtítulos da tela inicial, incluindo a imagem do card e seus respectivos subtítulos.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -7,18 +7,37 @@ export default function App() {
       {/* Header */}
       <View style={styles.header}>
         <View>
-          <Text>Cafe do Codigo</Text>
-          <Text>Seu cafe, uma linha por vez</Text>
+          <Text style={styles.headerTitle}>Cafe do Codigo</Text>
+          <Text style={styles.headerSubtitle}>Seu cafe, uma linha por vez</Text>
+        </View>
+
+        <View style={styles.avatarPlaceholder}>
+          <Ionicons name="person" size={20} color="#2f2d2c" ></Ionicons>
+        </View>
+      </View>
+      {/* Header */}
+
+      {/* Conteudo */}
+      <View style={styles.content}>
+        <View style={styles.greatingSection}>
+          <Text style={styles.greatingTitle}>Bom dia!</Text>
+          <Text style={styles.greatingSubtitle}>Quetal um cafe hoje?</Text>
         </View>
 
         <View>
-          <Ionicons name="person" size={20} color="#2f2d2c"></Ionicons>
+          <View>
+            <Image source={require('./assets/coffee.jpg')}/>
+            <Text>Capucino Especial</Text>
+            <Text>Cremoso e delicioso</Text>
+            <Text>R$ 12,90</Text>
+          </View>
         </View>
-
       </View>
+      {/* Conteudo */}
     </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -37,6 +56,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  
+  headerTitle: {
+    fontSize: 23,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+
+  headerSubtitle: {
+    fontSize: 14,
+    color: "#9b9b9b",
+    marginTop: 4,
+  },
+
+  avatarPlaceholder: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  content: {
+    paddingHorizontal: 24,
+  },
+
+  greatingSection: {
+    marginTop: 10,
+    marginBottom: 24,
+  },
+
+  greatingTitle: {
+      fontSize: 32,
+      fontWeight: '800',
+      color: "#2f2d2c"
+  },
+
+  greatingSubtitle: {
+      fontSize: 16,
+      color: "#9b9b9b",
+      marginTop: 8,
+  }
 
 });
